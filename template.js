9d51e7cecdd6faf91162a07582f6cbe0cc15cc62
tinytao1 <artiglove@icloud.com>
2026-03-16 22:37:17 -0700

    ok

--- template.js
+++ template.js
@@ -1,3 +1,34 @@
+/* HTML SECTION TEMPLATE
+
+    <!-- START SECTION  -->
+
+    <div class="section" id="s-section-div">
+
+      <div class="section-header" id="s-header">
+        <!-- START SECTION  HEADER -->
+
+        <h2 class="section-title" id="s-title">section title</h2>
+        <!-- <span class="section-counter" id="sc"> 0 </span> -->
+
+        <span
+          >(choose
+          <span class="choose-amount" id="s-choose-amount"> 0 </span>
+          )</span
+        >
+      </div>
+      <!-- END SECTION  HEADER -->
+
+      <div class="item-div">
+        <button id="s-" class="item-btn inactive-btn">item</button>
+        <span id="s-c" class="counter inactive-counter">0</span>
+      </div>
+
+        <!--
+// SECTION  /////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+// SECTION  /////////////////////////////////////////////////////////////////////////////////////
+-->
+
 "use strict";
 
 const s = {
